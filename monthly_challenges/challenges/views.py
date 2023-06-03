bfefb75a63439d3be6218ab36ba5d148f93071a3
anblackter <angel.henao@outlook.com>
2023-06-03 16:53:43 -0500

    A little change to a dictionary variable

--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -1,39 +1,32 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+monthly_challenges_dict = {
+    'january': 'January Challenge 1',
+    'february': 'February Challenge 1',
+    'march': 'March Challenge 1',
+    'april': 'April Challenge 1',
+    'may': 'May Challenge 1',
+    'june': 'June Challenge 1',
+    'july': 'July Challenge 1',
+    'august': 'August Challenge 1',
+    'september': 'September Challenge 1',
+    'october': 'October Challenge 1',
+    'november': 'November Challenge 1',
+    'december': 'December Challenge 1'
+}
+
 
 def index(request):
     return HttpResponse("This Works!")
 
+
 def monthly_challenges_by_number(request, month):
     return HttpResponse(month)
 
+
 def monthly_challenges(request, month):
-    challenge_text = None
-    if month == "january":
-        challenge_text = "January Challenge"
-    elif month == "february":
-        challenge_text = "February Challenge"
-    elif month == "march":
-        challenge_text = "March Challenge"
-    elif month == "april":
-        challenge_text = "April Challenge"
-    elif month == "may":
-        challenge_text = "May Challenge"
-    elif month == "june":
-        challenge_text = "June Challenge"
-    elif month == "july":
-        challenge_text = "July Challenge"
-    elif month == "august":
-        challenge_text = "August Challenge"
-    elif month == "september":
-        challenge_text = "September Challenge"
-    elif month == "october":
-        challenge_text = "October Challenge"
-    elif month == "november":
-        challenge_text = "November Challenge"
-    elif month == "december":
-        challenge_text = "December Challenge"
-    else:
-        return HttpResponse("This month is not supported")
-    return HttpResponse(challenge_text)
+    try:
+        return HttpResponse(monthly_challenges_dict[month])
+    except KeyError:
+        return HttpResponse("No challenges for " + month)
