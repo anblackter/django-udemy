3976c665db8aa069bdabd6b12b4aa15fad8bba76
anblackter <angel.henao@outlook.com>
2023-07-20 19:09:05 -0500

    Data & Models: Working with models.py, python3 manage.py shell. CRUD registers on Database

---
+++ book_store/book_outlet/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+from django.urls import reverse
+from django.utils.text import slugify
+
+# Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length=50)
+    rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+    author = models.CharField(null=True, max_length=100)    #null=True enables to populate the author as null, by default it's False
+    is_bestselling = models.BooleanField(default=False)
+    slug = models.SlugField(default="", null=False, db_index=True)
+
+    def get_absolute_url(self):
+        return reverse("book-detail", args=[self.slug])
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+
+    def __str__(self) -> str:
+        return f"{self.title} ({self.rating})"
+
