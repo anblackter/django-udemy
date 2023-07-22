1570929906afe3564d6b99cd468297d8608d4e53
anblackter <angel.henao@outlook.com>
2023-07-22 17:57:52 -0500

    Course Project - Building a Blog: Add Data Models, Admin and render Dynamic Data in the Blog Project

--- my_site/blog/admin.py
+++ my_site/blog/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Author, Tag, Post
 
 # Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    readonly_fields = ('slug',)
+    list_filter = ("author", "tags", "date",)
+    list_display = ("title", "date", "author",)
+
+admin.site.register(Author)
+admin.site.register(Tag)
+admin.site.register(Post, PostAdmin)
--- my_site/blog/models.py
+++ my_site/blog/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from django.urls import reverse
+from django.utils.text import slugify
 
 # Create your models here.
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email_address = models.EmailField(max_length=254)
+
+    def __str__(self) -> str:
+        return f'{self.first_name} {self.last_name}'
+
+class Tag(models.Model):
+    caption = models.CharField(max_length=500)
+
+    def __str__(self) -> str:
+        return f'{self.caption}'
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    excerpt = models.CharField(max_length=200)
+    image_name = models.CharField(max_length=50)
+    date = models.DateField(auto_now=True)
+    slug = models.SlugField(max_length=200, null=False, editable=False, unique=True, db_index=True)
+    content = models.TextField()
+    author = models.ForeignKey(Author, on_delete=models.SET_NULL, related_name='posts', null=True)
+    tags = models.ManyToManyField(Tag, null=False)
+
+    def get_absolute_url(self):
+        return reverse('post-detail-page', args=[self.slug])
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+
+    def __str__(self) -> str:
+        return f"{self.title} ({self.date})"
+
