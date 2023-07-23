d1b92cc998482c6918b4c1d51d22f3e54a29fa9c
anblackter <angel.henao@outlook.com>
2023-07-22 23:11:51 -0500

    Forms: Adding Clase Base Views

--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -1,13 +1,21 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.views import View
 
 from .forms import ReviewForm
-from .models import Review
 
 # Create your views here.
 
-def review(request):
-    if request.method == 'POST':
+
+class ReviewView(View):
+    def get(self, request):
+        form = ReviewForm()
+
+        return render(request, "reviews/review.html", {
+            "form": form
+        })
+
+    def post(self, request):
         form = ReviewForm(request.POST)
         ###example if you want to update and existing review
         # existing_data = Review.objects.get(pk=1)
@@ -22,13 +30,9 @@ def review(request):
             form.save()
             return HttpResponseRedirect("/thank-you")
 
-    else:
-        form = ReviewForm()
-
-    return render(request, "reviews/review.html", {
-        "form": form
-    })
-
+        return render(request, "reviews/review.html", {
+            "form": form
+        })
 
 def thank_you(request):
     return render(request, "reviews/thank_you.html")
