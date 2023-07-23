58bbc0e18994c284ae72dc88969edb9520575318
anblackter <angel.henao@outlook.com>
2023-07-22 22:51:14 -0500

    Forms: Adding forms.py, and Data model to store the data from the form to the database

--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -1,22 +1,28 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
+from .forms import ReviewForm
+from .models import Review
+
 # Create your views here.
 
 def review(request):
     if request.method == 'POST':
-        entered_username = request.POST['username']
+        form = ReviewForm(request.POST)
 
-        if entered_username == "" or len(entered_username) >= 100:
-            return render(request, "reviews/review.html", {
-                "has_error": True
-            })
+        if form.is_valid():
+            review = Review(
+                user_name=form.cleaned_data['user_name'],
+                review_text=form.cleaned_data['review_text'],
+                rating=form.cleaned_data['rating'])
+            review.save()
+            return HttpResponseRedirect("/thank-you")
 
-        print(entered_username)
-        return HttpResponseRedirect("/thank-you")
+    else:
+        form = ReviewForm()
 
     return render(request, "reviews/review.html", {
-        "has_error": False
+        "form": form
     })
 
 
