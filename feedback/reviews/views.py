a3200e9ab00e71fb925cc6f41b9caafbe000ef94
anblackter <angel.henao@outlook.com>
2023-07-22 23:05:06 -0500

    Forms: Adding Modelforms

--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -9,13 +9,17 @@ from .models import Review
 def review(request):
     if request.method == 'POST':
         form = ReviewForm(request.POST)
+        ###example if you want to update and existing review
+        # existing_data = Review.objects.get(pk=1)
+        # form = ReviewForm(request.POST, instance=existing_data)
 
         if form.is_valid():
-            review = Review(
-                user_name=form.cleaned_data['user_name'],
-                review_text=form.cleaned_data['review_text'],
-                rating=form.cleaned_data['rating'])
-            review.save()
+            # review = Review(
+            #     user_name=form.cleaned_data['user_name'],
+            #     review_text=form.cleaned_data['review_text'],
+            #     rating=form.cleaned_data['rating'])
+            # review.save()
+            form.save()
             return HttpResponseRedirect("/thank-you")
 
     else:
