415b1267f97f2ee4d5e7f8d7ff03d25bfb292422
anblackter <angel.henao@outlook.com>
2023-07-23 12:04:39 -0500

    File Upload: Serving the images on the web

--- feedback/profiles/models.py
+++ feedback/profiles/models.py
@@ -2,5 +2,9 @@ from django.db import models
 
 # Create your models here.
 
+#################################################
+# class UserProfile(models.Model):
+#     image = models.FileField(upload_to="images")
+
 class UserProfile(models.Model):
-    image = models.FileField(upload_to="images")
+    image = models.ImageField(upload_to="images")
--- feedback/profiles/views.py
+++ feedback/profiles/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponseRedirect
+from django.views.generic.edit import CreateView
+from django.views.generic import ListView
 
 from .forms import ProfileForm
 from .models import UserProfile
@@ -31,21 +33,33 @@ from .models import UserProfile
 #             "form": submitted_form
 #         })
 
-class CreateProfileView(View):
-    def get(self, request):
-        form = ProfileForm()
-        return render(request, "profiles/create_profile.html", {
-            "form": form
-        })
+####################################################################
+# class CreateProfileView(View):
+#     def get(self, request):
+#         form = ProfileForm()
+#         return render(request, "profiles/create_profile.html", {
+#             "form": form
+#         })
 
-    def post(self, request):
-        submitted_form = ProfileForm(request.POST, request.FILES)
+#     def post(self, request):
+#         submitted_form = ProfileForm(request.POST, request.FILES)
 
-        if submitted_form.is_valid():
-            profile = UserProfile(image=request.FILES["user_image"])
-            profile.save()
-            return HttpResponseRedirect("/profiles")
+#         if submitted_form.is_valid():
+#             profile = UserProfile(image=request.FILES["user_image"])
+#             profile.save()
+#             return HttpResponseRedirect("/profiles")
 
-        return render(request, "profiles/create_profile.html", {
-            "form": submitted_form
-        })
+#         return render(request, "profiles/create_profile.html", {
+#             "form": submitted_form
+#         })
+
+class CreateProfileView(CreateView):
+    template_name = "profiles/create_profile.html"
+    model = UserProfile
+    fields = "__all__"
+    success_url = "/profiles"
+
+class ProfilesView(ListView):
+    model = UserProfile
+    template_name = "profiles/user_profiles.html"
+    context_object_name = "profiles"
